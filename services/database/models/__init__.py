d5319d2d661fcbb4425469c7b3acc32e18d35b88
Absalom Payne <Apayne1987@gmail.com>
2026-05-26 00:03:25 -0700

    Add user database model

--- services/database/models/__init__.py
+++ services/database/models/__init__.py
@@ -1,11 +1,13 @@
 from services.database.models.execution_log import ExecutionLog
 from services.database.models.runtime import Runtime
 from services.database.models.task import Task
+from services.database.models.user import User
 from services.database.models.workflow import Workflow
 
 __all__ = [
     "ExecutionLog",
     "Runtime",
     "Task",
+    "User",
     "Workflow",
 ]
