aa2139518e7a9d81e0977ee6b69a7543848a18bb
Absalom Payne <Apayne1987@gmail.com>
2026-05-26 00:04:39 -0700

    Add users table migration

---
+++ services/database/migrations/versions/dc329d48e915_add_users_table.py
@@ -0,0 +1,25 @@
+"""add users table
+
+Revision ID: dc329d48e915
+Revises: 5a38499f6ff5
+Create Date: 2026-05-26 00:04:07.545345
+"""
+
+from collections.abc import Sequence
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'dc329d48e915'
+down_revision: str | None = '5a38499f6ff5'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
